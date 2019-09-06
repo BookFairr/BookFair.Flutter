dea63b072e154f45b9f6eaf723a9c67c1db96fba
Deandrew <deamoore07@gmail.com>
2019-09-06 00:13:53 -0500

    commit message

---
+++ android/app/src/main/kotlin/deandrew/flutter_bookfair/MainActivity.kt
@@ -0,0 +1,13 @@
+package deandrew.flutter_bookfair
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
